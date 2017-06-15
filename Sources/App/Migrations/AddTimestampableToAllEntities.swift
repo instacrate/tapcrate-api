fb95e5a69136a5f45a7ce0f1a31f6fef2e131347
Hakon Hanesand <hakon@abello.no>
2017-06-15 10:33:47 -0500

    opt

--- Sources/App/Migrations/AddTimestampableToAllEntities.swift
+++ Sources/App/Migrations/AddTimestampableToAllEntities.swift
@@ -15,7 +15,7 @@ struct AddTimestampableToAllEntities: Preparation {
 
     static func prepare(_ database: Database) throws {
         try database.modify(Subscription.self) { builder in
-            builder.parent(Customer.self)
+            builder.parent(Customer.self, optional: true)
             builder.date(Subscription.createdAtKey, default: "2017-04-28 14:46:17")
             builder.date(Subscription.updatedAtKey, default: "2017-04-28 14:46:17")
         }
