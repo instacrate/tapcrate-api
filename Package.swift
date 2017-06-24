4281fb2790d177e79f26078c810867750f13ac83
Hakon Hanesand <hakon@abello.no>
2017-06-24 15:10:13 -0500

    resolved

--- Package.swift
+++ Package.swift
@@ -1,19 +1,42 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "tapcrate-api",
-    dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 2),
-        .Package(url: "https://github.com/instacrate/jwt.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1)
+    products: [
+        .library(name: "lib", type: .dynamic, targets: ["lib"]),
+        .executable(name: "api", targets: ["api"])
     ],
-    exclude: [
-        "Config",
-        "Database",
-        "Public"
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/mysql-provider.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/auth-provider.git", from: "1.0.0"),
+        .package(url: "https://github.com/nodes-vapor/paginator.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(
+            name: "lib",
+            dependencies: [
+                .product(name: "Vapor"),
+                .product(name: "MySQLProvider"),
+                .product(name: "JWT"),
+                .product(name: "AuthProvider"),
+                .product(name: "Paginator")
+            ]
+        ),
+        .target(
+            name: "api",
+            dependencies: [
+                .target(name: "tapcrate-lib")
+            ],
+            exclude: [
+                "Config",
+                "Database",
+                "Public"
+            ]
+        )
     ]
 )
 
