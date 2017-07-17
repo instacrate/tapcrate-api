2ae47ac5f5f0b2dadab1e302f341c11739e99162
Hakon Hanesand <hakon@abello.no>
2017-07-17 11:05:29 -0500

    remove promise lib

--- Package.swift
+++ Package.swift
@@ -4,23 +4,27 @@ import PackageDescription
 
 let package = Package(
     name: "tapcrate-api",
+    products: [
+        .library(name: "lib", targets: ["lib"]),
+        .executable(name: "api", targets: ["api"])
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/mysql-provider.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/jwt.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/auth-provider.git", from: "1.0.0"),
-        .package(url: "https://github.com/nodes-vapor/paginator.git", from: "1.0.0"),
-        .package(url: "https://github.com/khanlou/Promise.git", from: "1.0.0")
+        .package(url: "https://github.com/nodes-vapor/paginator.git", from: "1.0.0")
     ],
     targets: [
         .target(
             name: "lib",
             dependencies: [
-                Target.Dependency(stringLiteral: "Vapor"),
-                Target.Dependency(stringLiteral: "MySQLProvider"),
-                Target.Dependency(stringLiteral: "JWT"),
-                Target.Dependency(stringLiteral: "AuthProvider"),
-                Target.Dependency(stringLiteral: "Paginator")
+                "Vapor",
+                "MySQLProvider",
+                "JWT",
+                "AuthProvider",
+                "Paginator",
+                "Promise"
             ],
             path: "Sources",
             exclude: [
@@ -32,7 +36,7 @@ let package = Package(
         .target(
             name: "api",
             dependencies: [
-                Target.Dependency(stringLiteral: "lib")
+                "lib"
             ]
         )
     ]   
